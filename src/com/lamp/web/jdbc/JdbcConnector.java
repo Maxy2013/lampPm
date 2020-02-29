7ef333a96fe2ec6306bf782d55a4f29901f3ba06
liya <15825949129@139.com>
2020-02-29 11:21:22 +0800

    jdbc

---
+++ src/com/lamp/web/jdbc/JdbcConnector.java
@@ -0,0 +1,63 @@
+package com.lamp.web.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+public class JdbcConnector {
+	private static final String URL="jdbc:mysql://localhost:3306/book_management";
+	private static final String USER="root";
+	private static final String PASSWORD="root";
+	private static Connection conn=null;
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		//加载驱动
+		Class.forName("com.mysql.jdbc.Driver");
+		//获取数据库连接
+	    try {
+			conn = DriverManager.getConnection(URL,USER,PASSWORD);
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	    PreparedStatement preparedStatement = null;
+//	    String sql = "select * from book";
+	    //获取PreparedStatement
+	    preparedStatement = conn.prepareStatement("select * from book");
+	    //执行sql,获取结果集
+	    ResultSet rs =preparedStatement.executeQuery();
+	    //遍历结果集,取出数据
+	   while(rs.next()) {
+		   String id = rs.getString(1);
+		   String name = rs.getString(2);
+		   String number = rs.getString(3);
+		   String author = rs.getString(4);
+		   //输出数据
+		   System.out.println(id+"\t"+name+"\t"+number+"\t"+author);
+	   }
+	   //关闭结果集
+	   rs.close();
+	   //关闭数据库连接
+	   preparedStatement.close();
+	   //关闭连接
+	   conn.close();
+	    
+	}
+	
+	static {
+		//加载驱动
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		//获取数据库连接
+			conn=DriverManager.getConnection(URL,USER,PASSWORD);
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConnection() {
+		return conn;
+	}
+}
