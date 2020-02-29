a65306fab84349498a63c5528388e49c9940e866
liya <15825949129@139.com>
2020-02-29 21:15:37 +0800

    增加条件查询

--- src/com/lamp/web/dao/CompanyDao.java
+++ src/com/lamp/web/dao/CompanyDao.java
@@ -41,4 +41,29 @@ public class CompanyDao {
 		}
 		return list;
 	}
+	//根据companyUnicode公司编码查询相关信息
+	public static List<Company> queryCompanyWithCompanyUnicode(String companyUnicode) {
+		connection = JdbcConnector.getConnection();
+		String sql = "select * from company_info";
+		List<Company> list = new ArrayList<Company>();
+		try {
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setString(2, companyUnicode);
+			ResultSet resultSet = ps.executeQuery(sql);
+			while (resultSet.next()) {
+				Company company=new Company();
+				company.setId(resultSet.getInt("id"));
+				company.setCompanyName(resultSet.getString("company_name"));
+				company.setCompanyUnicode(resultSet.getString("company_unicode"));
+				company.setAddress(resultSet.getString("address"));
+				company.setCreatedTime(resultSet.getDate("created_time"));
+				company.setModifiedTime(resultSet.getDate("modified_time"));
+				list.add(company);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
 }
